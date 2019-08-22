92663b21bf596730d0fc523e19bb3b3d1b0b59a9
Rounak Vyas <itsron143@gmail.com>
2019-08-23 05:24:55 +0530

    Added vision example 1

---
+++ KiwiPyConX/Code/create_thumbnail_brute_force.py
@@ -0,0 +1,30 @@
+# source: https://chriskiehl.com/article/parallelism-in-one-line
+
+from multiprocessing import Pool
+from PIL import Image
+import os
+
+SIZE = (75,75)
+SAVE_DIRECTORY = 'thumbs'
+
+def get_image_paths(folder):
+  return (os.path.join(folder, f)
+      for f in os.listdir(folder)
+      if 'jpeg' in f)
+
+def create_thumbnail(filename):
+  im = Image.open(filename)
+  im.thumbnail(SIZE, Image.ANTIALIAS)
+  base, fname = os.path.split(filename)
+  save_path = os.path.join(base, SAVE_DIRECTORY, fname)
+  im.save(save_path)
+
+if __name__ == '__main__':
+  folder = os.path.abspath(
+    'FODLER_NAME')
+  os.mkdir(os.path.join(folder, SAVE_DIRECTORY))
+
+  images = get_image_paths(folder)
+
+  for image in images:
+    create_thumbnail(image)
